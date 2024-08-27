907d97f8a8380806835c1dbfd237421b7177bc8c
立 <woticeli@gmail.com>
2024-08-27 16:40:59 +0800

    fix: apiBase dont need host

--- frontend/app/utils/constants.tsx
+++ frontend/app/utils/constants.tsx
@@ -1,4 +1,5 @@
 const apiBasePath = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:3000`;
-export const apiBaseUrl = `${apiBasePath}/api`;
+// export const apiBaseUrl = `${apiBasePath}/api`;
+export const apiBaseUrl = `/api`;
